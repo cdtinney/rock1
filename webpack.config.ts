c61211336b99a5b9d9755ddebc837fff05560a4e
Colin Tinney <colintinney@gmail.com>
2023-01-02 17:32:59 -0800

    chore: copy VoragoTag source code

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,19 +1,19 @@
 import alt1chain from "@alt1/webpack";
 import * as path from "path";
 
-var srcdir = path.resolve(__dirname, "./src/");
-var outdir = path.resolve(__dirname, "./dist/");
+const srcdir = path.resolve(__dirname, "./src/");
+const outdir = path.resolve(__dirname, "./dist/");
 
 //wrapper around webpack-chain, most stuff you'll need are direct properties,
 //more finetuning can be done at config.chain
 //the wrapper gives decent webpack defaults for everything alt1/typescript/react related
-var config = new alt1chain(srcdir, { ugly: false });
+const config = new alt1chain(srcdir, { ugly: false });
 
 //exposes all root level exports as UMD (as named package "testpackege" or "TEST" in global scope)
 config.makeUmd("testpackage", "TEST");
 
 //the name and location of our entry file (the name is used for output and can contain a relative path)
-config.entry("index", "./index.ts");
+config.entry("index", "./script.js");
 
 //where to put all the stuff
 config.output(outdir);
