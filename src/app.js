98f38a1f054fa43b5145e7a4ba2de1a5d6da5cfd
cdtinney <colintinney@gmail.com>
2023-02-27 20:11:50 -0800

    convert tabs to spaces

--- src/app.js
+++ src/app.js
@@ -6,8 +6,8 @@
  * @returns {String} color the bar should use
  */
 function getProgressBarColor(percent) {
-	const hue = (percent * 1.2).toString(10);
-	return "hsl(" + hue + ",75%,50%)";
+  const hue = (percent * 1.2).toString(10);
+  return "hsl(" + hue + ",75%,50%)";
 }
 
 function sanitisePercentage(integer) {
@@ -15,21 +15,21 @@ function sanitisePercentage(integer) {
 }
 
 function updateProgressBar(secondsLeft) {
-	const Settings = window.Rock1.Settings;
+  const Settings = window.Rock1.Settings;
 
-	const ticksLeft = Math.floor(secondsLeft / 0.6);
+  const ticksLeft = Math.floor(secondsLeft / 0.6);
 
-	if (Settings.displayType === Settings.DisplayType.Ticks) {
-		document.getElementById("progressBarText").innerHTML = "-" + ticksLeft + "t"
-	} else {
-		document.getElementById("progressBarText").innerHTML = "-" + secondsLeft + "s";
-	}
+  if (Settings.displayType === Settings.DisplayType.Ticks) {
+    document.getElementById("progressBarText").innerHTML = "-" + ticksLeft + "t"
+  } else {
+    document.getElementById("progressBarText").innerHTML = "-" + secondsLeft + "s";
+  }
 
-	const percent = sanitisePercentage(secondsLeft / 246 * 1000);
+  const percent = sanitisePercentage(secondsLeft / 246 * 1000);
 
-	const progressBarElem = document.getElementById("progressBarBar");
-	progressBarElem.style.width = percent + "%";
-	progressBarElem.style.backgroundColor = getProgressBarColor(percent);
+  const progressBarElem = document.getElementById("progressBarBar");
+  progressBarElem.style.width = percent + "%";
+  progressBarElem.style.backgroundColor = getProgressBarColor(percent);
 
   // TODO Change colour of text when on 2nd last tick
 
@@ -37,9 +37,9 @@ function updateProgressBar(secondsLeft) {
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 async function initialize() {
-	window.initializeSettings();
-	const timer = window.initializeTimer(updateProgressBar.bind(this));
-	const voragoImageDetect = await window.initializeVoragoImageDetect();
+  window.initializeSettings();
+  const timer = window.initializeTimer(updateProgressBar.bind(this));
+  const voragoImageDetect = await window.initializeVoragoImageDetect();
 
   if (!window.alt1) {
     console.error('alt1lib not found');
@@ -48,15 +48,15 @@ async function initialize() {
     return;
   }
 
-	setInterval(function() {
-		if (timer.isRunning) {
-			return;
-		}
+  setInterval(function() {
+    if (timer.isRunning) {
+      return;
+    }
 
-		const shouldStartTimer = voragoImageDetect.findZeroHpImage();
-		if (shouldStartTimer) {
-			timer.reset(24.6); // 24.6 seconds (40t) from zero HP to TC tick
-			timer.start(10); // Update every 10ms
-		}
-	}, 25); // 25ms
+    const shouldStartTimer = voragoImageDetect.findZeroHpImage();
+    if (shouldStartTimer) {
+      timer.reset(24.6); // 24.6 seconds (40t) from zero HP to TC tick
+      timer.start(10); // Update every 10ms
+    }
+  }, 25); // 25ms
 }
--- src/js/timer.js
+++ src/js/timer.js
@@ -1,51 +1,51 @@
 /* eslint-env browser */
 
 class Timer {
-	currentTime = 0;
-	isRunning = false
-	timerId = null;
-	callback = null;
-	startTime = new Date().getTime();
+  currentTime = 0;
+  isRunning = false
+  timerId = null;
+  callback = null;
+  startTime = new Date().getTime();
 
-	start(interval = 1000) {
-		if (this.isRunning) {
-			return;
-		}
+  start(interval = 1000) {
+    if (this.isRunning) {
+      return;
+    }
 
-		this.isRunning = true;
-		this.timerId = setInterval(() => {
-			this.currentTime = this.startTime - (new Date().getTime());
-			if (this.currentTime <=0) {
-				this.stop();
-				return;
-			}
+    this.isRunning = true;
+    this.timerId = setInterval(() => {
+      this.currentTime = this.startTime - (new Date().getTime());
+      if (this.currentTime <=0) {
+        this.stop();
+        return;
+      }
 
-			this._triggerCallback();
-		}, interval);
-	}
+      this._triggerCallback();
+    }, interval);
+  }
 
-	stop() {
-		if (!this.isRunning) {
-			return;
-		}
+  stop() {
+    if (!this.isRunning) {
+      return;
+    }
 
-		this.isRunning = false;
-		clearInterval(this.timerId);
-		this.callback(0);
-	}
+    this.isRunning = false;
+    clearInterval(this.timerId);
+    this.callback(0);
+  }
 
-	reset(newStartTimeInSeconds = 0) {
-		this.startTime = (new Date().getTime()) + (newStartTimeInSeconds * 1000);
-	}
+  reset(newStartTimeInSeconds = 0) {
+    this.startTime = (new Date().getTime()) + (newStartTimeInSeconds * 1000);
+  }
 
-	constructor(callback) {
-		this.callback = callback;
-	}
+  constructor(callback) {
+    this.callback = callback;
+  }
 
-	_triggerCallback() {
-		const secondsLeft = (Math.floor(this.currentTime / 600) * 0.6 ).toFixed(1);
-		this.callback(secondsLeft);
-	}
+  _triggerCallback() {
+    const secondsLeft = (Math.floor(this.currentTime / 600) * 0.6 ).toFixed(1);
+    this.callback(secondsLeft);
+  }
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -56,5 +56,5 @@ function initializeTimer(callback) {
 
   window.Rock1 = window.Rock1 || {};
   window.Rock1.Timer = new Timer(callback);
-	return window.Rock1.Timer;
+  return window.Rock1.Timer;
 }
